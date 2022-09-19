11dbba99d6d723442e33a0b76a8436b0a9c7def2
aoigj100a <aoigj100@gmail.com>
2022-09-19 20:42:13 +0800

    add : TodoPage and tailwindCss

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import TodoPage from './pages/TodoPage'
 
 function App() {
   return (
     <div className="App">
-      hello world.
+      <TodoPage/>
     </div>
   );
 }
