7e42490113f0512a0ebe93e8ce5d1d4733b10941
aoigj100a <aoigj100@gmail.com>
2022-10-07 20:45:14 +0800

    新增 HomeModal

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useState } from "react";
 import AddButton from "../../features/AddButton";
+import HomeModal from "./HomeModal";
 import { useAppSelector, useAppDispatch } from "../../redux/hooks";
 import {
   selectModalIsOpen,
@@ -8,52 +9,73 @@ import {
   closeModal,
 } from "../../redux/modalSlice/modalSlice";
 import { selectList, addTodo } from "../../redux/todoSlice/todoSlice";
+import { useForm } from "react-hook-form";
 // TODO 彈跳視窗
 // TODO 加入 list
 // TODO 把一個 todo 放到釘選位置
 // TODO 思考要釘選哪一種 todo
-const HomePage: React.FC = () => {
 
+const HomePage: React.FC = () => {
   const dispatch = useAppDispatch();
-  // const [isOpen, setIsOpen] = useState(false);
   const isOpen = useAppSelector(selectModalIsOpen);
   const list = useAppSelector(selectList);
-
-  console.log("list",list,"isOpen",isOpen)
-
+  const {
+    register,
+    handleSubmit,
+    watch,
+    reset,
+    formState: { errors },
+  } = useForm();
+  console.log("list", list, "isOpen", isOpen);
+  const onSubmit = (data) => {
+    // setList([...list, data]);
+    dispatch(addTodo(data));
+    if (isOpen) dispatch(closeModal());
+    reset();
+    console.log("list", list);
+  };
   return (
-    <section className="max-w-[1200px] mx-auto my-0 pt-[40px]">
-      <div className="flex items-center justify-between">
-        <AddButton
-        onClick={() => console.log("hi")}
-        />
-        <div className="flex flex-col justify-center ml-[8px] px-[8px]  w-[800px] pr-[8px]">
-          <div className="w-full flex">
-            <div className="w-2/12 flex items-center justify-center bg-gray-200 rounded-l-xl">
-              已釘選
-            </div>
-            <div className="bg-slate-50 w-full pl-[8px]">
-              <h3 className="">購物清單</h3>
-              <div className="text-sm">買蘋果、口罩、洗衣精</div>
+    <>
+      <HomeModal
+        isOpen={isOpen}
+        closeModal={() => dispatch(closeModal())}
+        register={register}
+        onSubmit={onSubmit}
+        handleSubmit={handleSubmit}
+        reset={reset}
+      />
+
+      <section className="max-w-[1200px] mx-auto my-0 pt-[40px]">
+        <div className="flex items-center justify-between">
+          <AddButton onClick={() => dispatch(openModal())} />
+          <div className="flex flex-col justify-center ml-[8px] px-[8px]  w-[800px] pr-[8px]">
+            <div className="w-full flex">
+              <div className="w-2/12 flex items-center justify-center bg-gray-200 rounded-l-xl">
+                已釘選
+              </div>
+              <div className="bg-slate-50 w-full pl-[8px]">
+                <h3 className="">購物清單</h3>
+                <div className="text-sm">買蘋果、口罩、洗衣精</div>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      <div className="pt-[20px]">
-        <ul className="flex justify-between">
-          <li className="p-[8px] mr-[16px] h-[600px] w-[450px] bg-gray-300">
-            鐵櫃
-          </li>
-          <li className="p-[8px] mr-[16px] h-[600px] w-[450px] bg-gray-300">
-            書櫃
-          </li>
-          <li className="p-[8px] mr-[16px] h-[600px] w-[450px] bg-gray-300">
-            桌子
-          </li>
-          <li className="p-[8px] h-[600px] w-[450px] bg-gray-300">冰箱</li>
-        </ul>
-      </div>
-    </section>
+        <div className="pt-[20px]">
+          <ul className="flex justify-between">
+            <li className="p-[8px] mr-[16px] h-[600px] w-[450px] bg-gray-300">
+              鐵櫃
+            </li>
+            <li className="p-[8px] mr-[16px] h-[600px] w-[450px] bg-gray-300">
+              書櫃
+            </li>
+            <li className="p-[8px] mr-[16px] h-[600px] w-[450px] bg-gray-300">
+              桌子
+            </li>
+            <li className="p-[8px] h-[600px] w-[450px] bg-gray-300">冰箱</li>
+          </ul>
+        </div>
+      </section>
+    </>
   );
 };
 
