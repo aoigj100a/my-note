b05956655b8c42c1f6cd7cdb17a945574d740297
aoigj100a <aoigj100@gmail.com>
2022-10-29 16:18:27 +0800

    add :logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Outlet, Link } from "react-router-dom";
 import Button from "../components/Button";
+import Logo from "./logo/logo";
 
 const Header = () => {
   return (
      <div className="flex justify-between items-center shadow px-[20px] py-[20px]">
      <h1 className="text-[30px] font-medium">
-       <Link to="/">MyNote</Link>
+       <Link to="/"><Logo /></Link>
      </h1>
      <div>
        <Button className="mr-[8px]">
